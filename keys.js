66b2bb18a7a1fcdeb5085140e45e944af438f77b
Austin Smith <austinsmith7531@gmail.com>
2019-05-05 13:06:11 -0400

    commit early and often

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID = "dff007447c424d1fa9fb8e52ad31b38e",
+  secret: process.env.SPOTIFY_SECRET = "81c88cff904e448286b813a2e3886088"
+};
--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
-var keys = require("keys.js");
+var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
